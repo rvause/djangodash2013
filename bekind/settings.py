b111f8a9772626d616ecf4241631a3fdf0c8db31
Rick Vause <rvause@gmail.com>
2013-09-28 17:00:34 +0200

    Add login with twitter

--- bekind/settings.py
+++ bekind/settings.py
@@ -144,6 +144,9 @@ INSTALLED_APPS = (
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+LOGIN_URL = '/'
+LOGOUT_URL = '/logout/'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
--- bekind/urls.py
+++ bekind/urls.py
@@ -4,8 +4,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(
+        r'^logout/',
+        'django.contrib.auth.views.logout',
+        {'next_page': '/'},
+        name='logout'
+    ),
     url(r'^', include('suggestions.urls', 'suggestions')),
     url(r'^', include('social.apps.django_app.urls', 'social')),
 )
