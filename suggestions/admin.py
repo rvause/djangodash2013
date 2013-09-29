10f8204056a601ee4136de5ae868374f8eb00701
Rick Vause <rvause@gmail.com>
2013-09-29 11:56:12 +0200

    Add some customisations to admin configuration for suggestions

--- suggestions/admin.py
+++ suggestions/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 from .models import Suggestion
+from .forms import SuggestionAdminForm
 
 
 class SuggestionAdmin(admin.ModelAdmin):
-    list_display = ('text', 'public')
-    fields = ('text', 'public')
+    form = SuggestionAdminForm
+    list_display = ('__str__', 'public')
+    fields = ('text', 'slug', 'public')
+    prepopulated_fields = {'slug': ('text',)}
+    list_filter = ('public',)
+    search_fields = ('text',)
 admin.site.register(Suggestion, SuggestionAdmin)
